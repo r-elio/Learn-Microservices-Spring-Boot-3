cab3efb19a4e75513f2699e8c9bc5f9048c02095
Rajan Elio <elio.recn@gmail.com>
2023-12-15 20:26:14 +0800

    minor fixes for chapter 8a

--- challenges-frontend/src/services/GameApiClient.ts
+++ challenges-frontend/src/services/GameApiClient.ts
@@ -1,5 +1,5 @@
 class GameApiClient {
-    static SERVER_URL = 'http://localhost:8081';
+    static SERVER_URL = 'http://localhost:8000';
     static GET_LEADERBOARD = '/leaders';
     static leaderBoard(): Promise<Response> {
         return fetch(GameApiClient.SERVER_URL + GameApiClient.GET_LEADERBOARD);
--- multiplication/src/main/java/microservices/book/multiplication/configuration/WebConfiguration.java
+++ multiplication/src/main/java/microservices/book/multiplication/configuration/WebConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     * More info: <a href="https://docs.spring.io/spring-framework/reference/web/webmvc-cors.html">webmvc-cors</a>
+     */
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:3000");
